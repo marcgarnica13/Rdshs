d5a1385c92ad4c5eb85e5fb2f2571ff468aac560
Marc Garnica Caparrós <marcgarnica13@gmail.com>
2022-12-23 18:56:28 +0100

    Fixing unicode codes in player names

--- data-raw/football.R
+++ data-raw/football.R
@@ -7,5 +7,9 @@ usethis::use_data(fb.epl.xg2122, overwrite = TRUE)
 fb.YYIR.running <- read.csv("data-raw/fb.YYIR.running.csv", stringsAsFactors = TRUE)
 usethis::use_data(fb.YYIR.running, overwrite = TRUE)
 
-fb.logs.player_position <- read.csv("data-raw/fb.logs.player_position.csv", stringsAsFactors = TRUE)
+fb.logs.player_position <- read.csv(
+  "data-raw/fb.logs.player_position.csv",
+  stringsAsFactors = F,
+)
+fb.logs.player_position$short_name <- stringi::stri_unescape_unicode(fb.logs.player_position$short_name)
 usethis::use_data(fb.logs.player_position, overwrite = TRUE)
